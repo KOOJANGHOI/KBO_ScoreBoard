31502f8b05d62ac9cefd5a38c40300c5da55d570
kjanghoi <kjanghoi@gmail.com>
2017-06-06 21:25:02 +0900

    ui

---
+++ KBO/KBO/kbo_3/PredictView.swift
@@ -0,0 +1,18 @@
+//
+//  PredictView.swift
+//  kbo_3
+//
+//  Created by 구장회 on 2017. 6. 6..
+//  Copyright © 2017년 구장회. All rights reserved.
+//
+
+import UIKit
+
+class PredictView: UIView {
+
+    @IBInspectable var cornerRadius:CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+}
